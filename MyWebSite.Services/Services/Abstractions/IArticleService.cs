fd03cf40498cad3aaed2aa038188d24a8c067da7
Onur Kırık <kirikonurr@gmail.com>
2023-02-15 04:05:02 +0300

    UnitOfWorks structure is comleted.

---
+++ MyWebSite.Services/Services/Abstractions/IArticleService.cs
@@ -0,0 +1,14 @@
+﻿using MyWebSite.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebSite.Services.Services.Abstractions
+{
+    public interface IArticleService
+    {
+        Task<List<Article>> GetAllArticlesAsync();
+    }
+}
---
+++ MyWebSite.Services/Services/Concrete/ArticleService.cs
@@ -0,0 +1,25 @@
+﻿using MyWebSite.DATA.UnitOfWorks;
+using MyWebSite.Entity.Entities;
+using MyWebSite.Services.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebSite.Services.Services.Concrete
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ArticleService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<List<Article>> GetAllArticlesAsync()
+        {
+            return await _unitOfWork.GetRepository<Article>().GetAllAsync();
+        }
+    }
+}
