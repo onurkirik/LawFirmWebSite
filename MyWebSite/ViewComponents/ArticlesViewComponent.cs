eadddd1206afa0ef2504069c3691527b59d9385d
Onur Kırık <kirikonurr@gmail.com>
2023-02-20 08:48:51 +0300

    Project has done.

---
+++ MyWebSite/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MyWebSite.Entity.Entities;
+using MyWebSite.Service.Services.Abstractions;
+
+namespace MyWebSite.Web.ViewComponents
+{
+    public class ArticlesViewComponent : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public ArticlesViewComponent(IArticleService articleService, ICategoryService categoryService, IMapper mapper)
+        {
+            _articleService = articleService;
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var articles = await _articleService.GetAllArticlesWithCategoryNonDeletedAsync();
+            articles.OrderByDescending(a => a.CreatedDate);
+
+            return View(articles);
+        }
+    }
+}
---
+++ MyWebSite/ViewComponents/SuggestArticleViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MyWebSite.DATA.Context;
+using MyWebSite.Entity.Entities;
+using MyWebSite.Service.Services.Abstractions;
+
+namespace MyWebSite.Web.ViewComponents
+{
+    public class SuggestArticleViewComponent : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+        private readonly ApplicationDbContext _db;
+        private int listLength;
+
+        public SuggestArticleViewComponent(IArticleService articleService, ICategoryService categoryService, IMapper mapper, ApplicationDbContext db)
+        {
+            _articleService = articleService;
+            _categoryService = categoryService;
+            _mapper = mapper;
+            _db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(Guid articleId)
+        {
+            var articles = _db.Articles.OrderByDescending(a => a.CreatedDate).Where(a => a.ID != articleId).ToList();
+            listLength = articles.Count();
+            articles = articles.Take(listLength / 2).ToList();
+
+            return View(articles);
+        }
+    }
+}
